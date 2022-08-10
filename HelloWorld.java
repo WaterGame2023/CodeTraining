a6d6c5eb30581f233e8927b4b0c6245cd5e09aac
SaltWater <97995798+Sh0rtMort@users.noreply.github.com>
2022-08-10 18:36:09 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Gabby's Moms House");
+        System.out.println(one + two);
+    }
+    public void RandomCode() {
+        String one = "im At your";
+        String two = "Mom or dads House";
+       
     }
 }
